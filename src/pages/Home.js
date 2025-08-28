e1e8b181d75387ebe6aead8aa5369099501bfd9a
Heona Liu <heonaliu@Heonas-Mac-mini.local>
2025-08-28 15:00:46 -0400

    Site made with React text at the bottom of page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,35 +3,38 @@ import "./Home.css";
 import ScrollingText from "../components/ScrollingText";
 
 const Home = () => {
-  const thingsIAm = [
-    "a ballet dancer",
-    "a pianist",
-    "a high schooler",
-    "a programmer",
-    "a badminton player",
-    "a digital artist",
-  ];
-
   return (
     <div className="home-container">
       <h1 className="center-text">Heona Liu</h1>
       <h2
         style={{ textAlign: "center", fontSize: "2.5rem", marginTop: "20px" }}
       >
-        I am
+        I am a
       </h2>
       <ScrollingText
         items={[
-          "a ballet dancer",
-          "a pianist",
-          "a high schooler",
-          "a programmer",
-          "a badminton player",
-          "a digital artist",
+          "Digital Artist",
+          "Pianist",
+          "High Schooler",
+          "Exchange Student",
+          "Badminton Player",
+          "Ballet Dancer",
+          "Graphics Designer",
+          "YouTuber",
+          "Video Editor",
         ]}
-        displayTime={2000} /* how long the item stays visible */
-        transitionTime={800} /* fade in/out duration */
+        displayTime={2000}
+        transitionTime={800}
       />
+
+      <div className="homepage-footer">
+        <p>
+          Site made with{" "}
+          <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+            React
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
