384481ddb72e21ef2c6507ae32e8c298264694dc
Heona Liu <heonaliu@Heonas-Mac-mini.local>
2025-08-28 13:42:58 -0400

    Written descriptions for programming projects page.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import ProjectCard from "../components/ProjectCard";
 import project1 from "../assets/images/project1.png";
 import project2 from "../assets/images/project2.png";
+import "./Projects.css";
 
 const Projects = () => {
   const projects = [
@@ -20,13 +21,35 @@ const Projects = () => {
   ];
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h2>Projects</h2>
-      <p>Overview of my projects combining art and coding.</p>
-
-      <h3>Early Programming</h3>
-      <p>Summary about my early programming projects...</p>
-
+    <div className="project-text" style={{ padding: "20px" }}>
+      <h2 style={{ fontSize: "3rem", fontWeight: "700" }}>My Projects</h2>
+      <p>
+        Hey! This is a place to put my projects. While I don't have many recent
+        projects at the moment, you can consider this website to be a project in
+        of itself. In high school I've taken Intro to Computer Science and
+        Computer Programming classes. Currently, I'm in the process of
+        rebuilding my passion for programming.
+      </p>
+      <h2 style={{ fontSize: "2rem", fontWeight: "700" }}>Early Programming</h2>
+      <p>
+        Since I was 7, my dad introduced me to Scratch! Inspired by projects, I
+        spent the majority of my time on there making projects. Throughout my
+        time on Scratch, I hopped around different accounts. I have a net total
+        of around 1600+ followers overall. Since 6th grade, my dad introduced me
+        to Python.{" "}
+      </p>{" "}
+      <p>
+        Due to the pandemic, my family made the decision to go homeschooled for
+        7th grade. During this time, I followed course, and getting exposed to
+        React.js. Meanwhile, I also liked to experiement playing around with
+        creating a Discord bot with Python by following YouTube videos. Later in
+        2021, I enrolled in STEM's Python course; in 2022, I enrolled in their
+        Java course for a semester. course for a semester.
+      </p>
+      <p>
+        In 8th grade, I went back to in-person school and took a pause in my
+        development in programming, which I realize was a big mistake.
+      </p>
       <div
         style={{
           display: "flex",
