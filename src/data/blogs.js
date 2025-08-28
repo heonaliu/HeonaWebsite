bc8911d8e118ff82b33f0f9db38d16e2e9d5850b
Heona Liu <heonaliu@Heonas-Mac-mini.local>
2025-08-28 17:28:36 -0400

    Insert blog image and modify blog 2 description

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -8,14 +8,14 @@ const blogsData = [
     image: blog1,
     date: "August 25, 2025",
     summary: "Built from an idea and the persistance to fulfill it.",
-    content: `Today was the day the idea was launched! \n\n The night before, my dad came in and told me about vibe coding. I was intrigued, yet hesitant. I haven't touched any kind of serious development programming since 7th grade and was constantly worried that, without relearning whole new React course, I wouldn't be able to program anything again. This doubt is what held me back and made me to believe that programming wasn't cut out for me. But there is so much that is unknown in the programming world that it is almost impossible for even the best developer to know everything. Vibe coding was essentially talking out your ideas and then an AI modal will comprehend that into executable code. As someone who struggled with writing code, this seemed like a win-win for me.\n\n That night, this idea of vibe coding tossed around in my head. I really wanted to try it out. So the next morning, I took a the next big step and opened ChatGPT and ran my first npm command in 4 years. `,
+    content: `The night before, my dad came in and told me about vibe coding. I was intrigued yet hesitant. I hadn't touched any kind of serious development programming since 7th grade and was constantly worried that, without relearning a whole new React course, I wouldn't be able to program anything again. This doubt held me back and made me believe that programming wasn't cut out for me. But there is so much unknown in the programming world that it is almost impossible for even the best developer to know everything.\n\nVibe coding was essentially talking out your ideas and then having an AI model translate them into executable code. As someone who struggled with writing code, this seemed like a win-win for me.\n\nThat night, the idea of vibe coding kept tossing around in my head. I really wanted to try it out. So the next morning, I took the next big step: I opened ChatGPT and ran my first npm command in 4 years.`,
   },
   {
     id: 2,
     title: "Launched!",
     image: blog2,
-    date: "August 26, 2025",
-    summary: "After several days...",
+    date: "August 28, 2025",
+    summary: "After several days of vibing and programming...",
     content: "Full content of the second blog goes here...",
   },
 ];
