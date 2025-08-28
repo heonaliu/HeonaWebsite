a22addd4ada18c777f5fa30cda18153eb2c7e10e
Heona Liu <heonaliu@Heonas-Mac-mini.local>
2025-08-28 15:42:34 -0400

    Blog 1 content

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -4,15 +4,15 @@ import blog2 from "../assets/images/blog2.png";
 const blogsData = [
   {
     id: 1,
-    title: "Website Established",
+    title: "The Idea: Establishment",
     image: blog1,
     date: "August 25, 2025",
     summary: "Built from an idea and the persistance to fulfill it.",
-    content: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean \n \n sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.`,
+    content: `Today was the day the idea was launched! \n\n The night before, my dad came in and told me about vibe coding. I was intrigued, yet hesitant. I haven't touched any kind of serious development programming since 7th grade and was constantly worried that, without relearning whole new React course, I wouldn't be able to program anything again. This doubt is what held me back and made me to believe that programming wasn't cut out for me. But there is so much that is unknown in the programming world that it is almost impossible for even the best developer to know everything. Vibe coding was essentially talking out your ideas and then an AI modal will comprehend that into executable code. As someone who struggled with writing code, this seemed like a win-win for me.\n\n That night, this idea of vibe coding tossed around in my head. I really wanted to try it out. So the next morning, I took a the next big step and opened ChatGPT and ran my first npm command in 4 years. `,
   },
   {
     id: 2,
-    title: "Website Published",
+    title: "Launched!",
     image: blog2,
     date: "August 26, 2025",
     summary: "After several days...",
