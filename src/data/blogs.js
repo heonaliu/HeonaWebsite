fff5c8eba030ada082f44a826077dbd00cf6b860
Heona Liu <heonaliu@Heonas-Mac-mini.local>
2025-08-28 13:12:48 -0400

    Modify blog content

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -4,18 +4,18 @@ import blog2 from "../assets/images/blog2.png";
 const blogsData = [
   {
     id: 1,
-    title: "My First Blog",
+    title: "Website Established",
     image: blog1,
     date: "August 25, 2025",
-    summary: "This is a short summary of my first blog.",
+    summary: "Built from an idea and the persistance to fulfill it.",
     content: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean \n \n sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.`,
   },
   {
     id: 2,
-    title: "Soon To Come",
+    title: "Website Published",
     image: blog2,
     date: "August 26, 2025",
-    summary: "A summary of my second blog.",
+    summary: "After several days...",
     content: "Full content of the second blog goes here...",
   },
 ];
